1fb93eceae4e8eadda71c2315b2aa8c571811d85
VistritPandey <56611775+VistritPandey@users.noreply.github.com>
2020-06-23 21:35:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-#File:        main.js
-#Purpose:     Main page in java
-#Author:      Artem Yakupov
-#Date:        1 March, 2020
-#Dependency:  ------------
 $(document).ready(function(){
     $('.header').height($(window).height());
   })
