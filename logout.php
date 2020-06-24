7de3f8cd823c95609ab8cbd35e3efb2eec4a4c75
VistritPandey <56611775+VistritPandey@users.noreply.github.com>
2020-06-23 21:34:30 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,3 @@
-#File:        logout.php
-#Purpose:     Ending a session in order to allow the user to log out
-#Author:      Artem Yakupov
-#Date:        10 May, 2020
-#Dependency:  ------------
 <?php
 session_start();
 session_destroy();
